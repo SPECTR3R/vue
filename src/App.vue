b8c29f7d98ca1d6d530508415c0eb6cb7015a031
Armando del Río (Adrio) <adrio.coder@gmail.com>
2022-01-10 08:22:55 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <div id="v-step-0">
+      You can use IDs
+    </div>
+    <div class="v-step-1">
+      classes
+    </div>
+    <div data-v-step="2">
+      or the "data-v-step" attr
+    </div>
+
+    <v-tour name="myTour" :steps="steps"></v-tour>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  data () {
+      return {
+        steps: [
+          {
+            target: '#v-step-0',  // We're using document.querySelector() under the hood
+            content: `Discover <strong>Vue Tour</strong>!`
+          },
+          {
+            target: '.v-step-1',
+            content: 'An awesome plugin made with Vue.js!'
+          },
+          {
+            target: '[data-v-step="2"]',
+            content: 'Try it, you\'ll love it!<br>You can put HTML in the steps and completely customize the DOM to suit your needs.',
+            params: {
+              placement: 'top'
+            }
+          }
+        ]
+      }
+    },
+    mounted: function () {
+      this.$tours['myTour'].start()
+    },
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
